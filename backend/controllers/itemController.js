ebd9c0e053ad829e8c2810d347f9f7874c4531ab
adya tiwari <adya.tiwari20@st.niituniversity.in>
2024-08-09 16:40:16 +0530

    Initial commit

---
+++ backend/controllers/itemController.js
@@ -0,0 +1,14 @@
+const Item = require('../models/item');
+
+const getItems = async (req, res) => {
+    try {
+        const items = await Item.find();
+        res.json(items);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    getItems,
+};
